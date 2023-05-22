3543f92c2938208a912e8602987d4172cdc5225c
AlexanderSlokov <102212788+AlexanderSlokov@users.noreply.github.com>
2023-05-22 18:29:50 +0700

    Set X-Frame Option ON

---
+++ src/main/java/com/ShoeShopProject/filter/XFrameHeaderFilter.java
@@ -0,0 +1,22 @@
+package com.ShoeShopProject.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class XFrameHeaderFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        ((HttpServletResponse) resp).setHeader("x-frame-options", "allow");
+        chain.doFilter(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
